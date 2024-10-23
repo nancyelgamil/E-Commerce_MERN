e5b528f9da06a9cf5b6d7194f4e22fcba1cbf226
nancyelgamil <nancyelgamil@gmail.com>
2024-10-23 18:20:38 +0200

    Product feature

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoute";
-
+import { seedInitialProducts } from "./services/productService";
+import productRoute from "./routes/productRoute";
 const app = express();
 const port = 3001;  //Express Port
 
 app.use(express.json())
+
 //Connection to MongoDB
 mongoose
   .connect("mongodb://localhost:27017/ecommerce")
   .then(() => console.log("Mongo connected!"))
   .catch((err) => console.log("Failed to connect!", err));
 
+  // Seed the products to database
+seedInitialProducts();
+
 app.use('/user', userRoute)
+app.use('/product', productRoute)
 
 //express listen port
 app.listen(port, () => {
---
+++ src/services/productService.ts
@@ -0,0 +1,18 @@
+import productModel from "../models/productModel";
+
+export const getAllProducts = async () => {
+    return await productModel.find();
+}
+
+export const seedInitialProducts = async () => {
+    const products = [
+        { title: "Dell Laptop", image: "https://m.media-amazon.com/images/I/61ESd8sr8kL._AC_UF350,350_QL80_.jpg", price: 949, stock: 10 },
+
+    ];
+
+    const existingProducts = await getAllProducts();
+
+    if(existingProducts.length === 0) {
+        await productModel.insertMany(products)
+    }
+};
